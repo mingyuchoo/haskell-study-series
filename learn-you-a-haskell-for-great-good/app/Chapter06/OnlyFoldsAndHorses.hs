8112dedfe451aba866d90a680af9fef69d01f6ea
mingyuchoo <mingyuchoo@gmail.com>
2021-06-02 07:12:45 +0900

    macos

--- learn-you-a-haskell-for-great-good/app/Chapter06/OnlyFoldsAndHorses.hs
+++ learn-you-a-haskell-for-great-good/app/Chapter06/OnlyFoldsAndHorses.hs
@@ -23,12 +23,8 @@ elem' :: (Eq a) => a -> [a] -> Bool
 elem' y ys = foldl (\acc x -> if x == y then True else acc) False ys
 
 
--- | map'
+-- | map''
 --
-map' :: (a -> b) -> [a] -> [b]
-map' f xs = foldr (\x acc -> f x : acc) [] xs
-
-
-add :: [Int] -> Int
-add xs = xs:[x]
+map'' :: (a -> b) -> [a] -> [b]
+map'' f xs = foldr (\x acc -> f x : acc) [] xs
 
