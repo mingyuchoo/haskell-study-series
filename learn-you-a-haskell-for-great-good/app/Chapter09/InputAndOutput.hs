176846115427de1f175d017aa1ee755445ef3ec5
mingyuchoo <mingyuchoo@gmail.com>
2021-06-20 16:28:49 +0900

    t570

--- learn-you-a-haskell-for-great-good/app/Chapter09/InputAndOutput.hs
+++ learn-you-a-haskell-for-great-good/app/Chapter09/InputAndOutput.hs
@@ -132,12 +132,12 @@
 --         main
 
 
-import Control.Monad
-main = do
-    c <- getChar
-    when (c /= ' ') $ do
-        putChar c
-        main
+-- import Control.Monad
+-- main = do
+--     c <- getChar
+--     when (c /= ' ') $ do
+--         putChar c
+--         main
 
 
 {-- sequence --}
@@ -358,7 +358,56 @@ I think you need a new one!
 
 
 
+{-- Command line arguments --}
 
 
+-- import System.Environment
+-- import Data.List
+-- main = do
+--     args <- getArgs
+--     progName <- getProgName
+--     putStrLn "The arguments are:"
+--     mapM putStrLn args
+--     putStrLn "The program name is:"
+--     putStrLn progName
 
+
+import System.Environment
+import System.Directory
+import System.IO
+import Data.List
+
+dispatch :: [(String, [String] -> IO ())]
+dispatch =  [ ("add",    add)
+            , ("view",   view)
+            , ("remove", remove)]
+
+main = do
+    (command:args) <- getArgs
+    let (Just action) = lookup command dispatch
+    action args
+
+add :: [String] -> IO ()
+add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+
+view :: [String] -> IO ()
+view [fileName] = do
+    contents <- readFile fileName
+    let todoTasks = lines contents
+        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+    putStr $ unlines numberedTasks
+
+remove :: [String] -> IO ()
+remove [fileName, numberString] = do
+    handle <- openFile fileName ReadMode
+    (tempName, tempHandle) <- openTempFile "." "temp"
+    contents <- hGetContents handle
+    let number = read numberString
+        todoTasks = lines contents
+        newTodoItems = delete (todoTasks !! number) todoTasks
+    hPutStr tempHandle $ unlines newTodoItems
+    hClose handle
+    hClose tempHandle
+    removeFile fileName
+    renameFile tempName fileName
 
