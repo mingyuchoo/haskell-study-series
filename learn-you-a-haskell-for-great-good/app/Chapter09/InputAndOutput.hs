0937915fa22e086d9144e93f1bffcfa70d099752
mingyuchoo <mingyuchoo@gmail.com>
2021-06-21 16:50:08 +0900

    Update InputAndOutput.hs

--- learn-you-a-haskell-for-great-good/app/Chapter09/InputAndOutput.hs
+++ learn-you-a-haskell-for-great-good/app/Chapter09/InputAndOutput.hs
@@ -2,11 +2,14 @@
 -- (
 -- 
 -- ) where
+{-- ------------------------------------------------------------------------ --}
 -- main = putStrLn "Hello, World!"
+{-- ------------------------------------------------------------------------ --}
 -- main = do
 --     putStrLn "Hello, what's your name?"
 --     name <- getLine
 --     putStrLn ("Hey " ++ name ++ ", you rock!")
+{-- ------------------------------------------------------------------------ --}
 -- import Data.Char
 -- main = do
 --     putStrLn "What's your first name?"
@@ -19,6 +22,7 @@
 --     putStrLn $ "hey " ++ bigFirstName ++ " " ++ bigLastName ++ ", how are you?"
 -- reverseWords :: String -> String
 -- reverseWords = unwords . map reverse . words
+{-- ------------------------------------------------------------------------ --}
 -- main = do
 --     line <- getLine
 --     if null line
@@ -26,6 +30,7 @@
 --         else do
 --             putStrLn $ reverseWords line
 --             main
+{-- ------------------------------------------------------------------------ --}
 -- main = do
 --     return ()
 --     return "HAHAHA"
@@ -33,39 +38,47 @@
 --     return "BLAH BLAH BLAH BLAH"
 --     return 4
 --     putStrLn line
+{-- ------------------------------------------------------------------------ --}
 -- main = do
 --     a <- return "hell"
 --     b <- return "yeah!"
 --     putStrLn $ a ++ " " ++ b
+{-- ------------------------------------------------------------------------ --}
 -- main = do
 --     let a = "hell"
 --         b = "yeah"
 --     putStrLn $ a ++ " " ++ b
+{-- ------------------------------------------------------------------------ --}
 {-- putStr --}
 -- main = do   putStr "Hey, "
 --             putStr "I'm "
 --             putStrLn "Andy!"
+{-- ------------------------------------------------------------------------ --}
 {-- putchar --}
 -- main = do putChar 't'
 --           putChar 'e'
 --           putChar 'h'
+{-- ------------------------------------------------------------------------ --}
 {-- print --}
 -- main = do print True
 --           print 2
 --           print "haha"
 --           print 3.2
 --           print [3,4,3]
+{-- ------------------------------------------------------------------------ --}
 {-- when --}
 -- import Control.Monad
 -- main = do
 --     input <- getLine
 --     when (input == "SWORDFISH") $ do
 --         putStrLn input
+{-- ------------------------------------------------------------------------ --}
 -- main = do
 --     input <- getLine
 --     if (input == "SWORDFISH")
 --         then putStrLn input
 --         else return ()
+{-- ------------------------------------------------------------------------ --}
 -- main = do
 --     c <- getChar
 --     if c /= ' '
@@ -74,27 +87,32 @@
 --             main
 --         else 
 --             return ()
+{-- ------------------------------------------------------------------------ --}
 -- import Control.Monad
 -- main = do
 --     c <- getChar
 --     when (c /= ' ') $ do
 --         putChar c
 --         main
+{-- ------------------------------------------------------------------------ --}
 -- import Control.Monad
 -- main = do
 --     c <- getChar
 --     when (c /= ' ') $ do
 --         putChar c
 --         main
+{-- ------------------------------------------------------------------------ --}
 {-- sequence --}
 -- main = do
 --     a <- getLine
 --     b <- getLine
 --     c <- getLine
 --     print [a,b,c]
+{-- ------------------------------------------------------------------------ --}
 -- main = do
 --     rs <- sequence [getLine, getLine, getLine]
 --     print rs
+{-- ------------------------------------------------------------------------ --}
 {-- forever --}
 -- import Control.Monad
 -- import Data.Char
@@ -102,6 +120,7 @@
 --     putStr "Give me some input: "
 --     l <- getLine
 --     putStrLn $ map toUpper l
+{-- ------------------------------------------------------------------------ --}
 {-- forM --}
 -- import Control.Monad
 -- main = do
@@ -111,6 +130,7 @@
 --         return color)
 --     putStrLn "The colors that you associate with 1,2,3 and 4 are: "
 --     mapM putStrLn colors
+{-- ------------------------------------------------------------------------ --}
 {-- input redirection --}
 -- -- if you want to quit, type Ctr+d
 -- import Control.Monad
@@ -119,10 +139,12 @@
 --     putStr "Give me some input: "
 --     l <- getLine
 --     putStrLn $ map toUpper l
+{-- ------------------------------------------------------------------------ --}
 -- import Data.Char
 -- main = do
 --     contents <- getContents
 --     putStr $ map toUpper contents     -- ==  putStr (map toUpper contents)
+{-- ------------------------------------------------------------------------ --}
 -- main = do
 --     contents <- getContents
 --     putStr (shortLinesOnly contents)
@@ -133,6 +155,7 @@
 --         shortLines = filter (\line -> length line < 10) allLines
 --         result     = unlines shortLines
 --     in  result
+{-- ------------------------------------------------------------------------ --}
 -- main = interact shortLinesOnly
 -- shortLinesOnly :: String -> String
 -- shortLinesOnly input =
@@ -140,13 +163,16 @@
 --         shortLines = filter (\line -> length line < 10) allLines
 --         result     = unlines shortLines
 --     in  result
+{-- ------------------------------------------------------------------------ --}
 -- main = interact $ unlines . filter ((<10) . length) . lines
+{-- ------------------------------------------------------------------------ --}
 -- main = interact respondPalindromes
 -- respondPalindromes contents = unlines (map (\xs -> if isPalindrome xs then "palindrome" else "not a palindrome") (lines contents))
 --     where isPalindrome xs = xs == reverse xs
 -- main = interact respondPalindromes
 -- respondPalindromes = unlines . map (\xs -> if isPalindrome xs then "palindrome" else "not a palindrome") . lines
 --     where isPalindrome xs = xs == reverse xs
+{-- ------------------------------------------------------------------------ --}
 {-- // girlfriend.txt
 
 Hey! Hey! You! You!
@@ -161,11 +187,13 @@ I think you need a new one!
 --     contents <- hGetContents handle
 --     putStr contents
 --     hClose handle
+{-- ------------------------------------------------------------------------ --}
 -- import System.IO
 -- main = do
 --     withFile "girlfriend.txt" ReadMode (\handle -> do
 --         contents <- hGetContents handle
 --         putStr contents)
+{-- ------------------------------------------------------------------------ --}
 -- import System.IO
 -- main = do
 --     withFile' "girlfriend.txt" ReadMode (\handle -> do
@@ -178,30 +206,36 @@ I think you need a new one!
 --     result <- f handle
 --     hClose handle
 --     return result
+{-- ------------------------------------------------------------------------ --}
 -- import System.IO
 -- main = do
 --     contents <- readFile "girlfriend.txt"
 --     putStr contents
+{-- ------------------------------------------------------------------------ --}
 -- import System.IO
 -- import Data.Char
 -- main = do
 --     contents <- readFile "girlfriend.txt"
 --     writeFile "girlfriendcaps.txt" (map toUpper contents)
+{-- ------------------------------------------------------------------------ --}
 -- import System.IO
 -- main = do
 --     todoItem <- getLine
 --     appendFile "todo.txt" (todoItem ++ "\n")
+{-- ------------------------------------------------------------------------ --}
 -- import System.IO
 -- main = do
 --     withFile "todo.txt" ReadMode (\handle -> do
 --         contents <- hGetContents handle
 --         putStr contents)
+{-- ------------------------------------------------------------------------ --}
 -- import System.IO
 -- main = do
 --     withFile "todo.txt" ReadMode (\handle -> do
 --         hSetBuffering handle $ BlockBuffering (Just 2048)
 --         contents <- hGetContents handle
 --         putStr contents)
+{-- ------------------------------------------------------------------------ --}
 -- import System.IO
 -- import System.Directory
 -- import Data.List
@@ -222,6 +256,7 @@ I think you need a new one!
 --     hClose tempHandle
 --     removeFile "todo.txt"
 --     renameFile tempName "todo.txt"
+{-- ------------------------------------------------------------------------ --}
 {-- Command line arguments --}
 -- import System.Environment
 -- import Data.List
@@ -232,40 +267,42 @@ I think you need a new one!
 --     mapM putStrLn args
 --     putStrLn "The program name is:"
 --     putStrLn progName
-import Data.List
-import System.Directory
-import System.Environment
-import System.IO
-
-dispatch :: [(String, [String] -> IO ())]
-dispatch = [("add", add), ("view", view), ("remove", remove)]
-
-main = do
-  (command:args) <- getArgs
-  let (Just action) = lookup command dispatch
-  action args
-
-add :: [String] -> IO ()
-add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
-
-view :: [String] -> IO ()
-view [fileName] = do
-  contents <- readFile fileName
-  let todoTasks = lines contents
-      numberedTasks =
-        zipWith (\n line -> show n ++ " - " ++ line) [0 ..] todoTasks
-  putStr $ unlines numberedTasks
-
-remove :: [String] -> IO ()
-remove [fileName, numberString] = do
-  handle <- openFile fileName ReadMode
-  (tempName, tempHandle) <- openTempFile "." "temp"
-  contents <- hGetContents handle
-  let number = read numberString
-      todoTasks = lines contents
-      newTodoItems = delete (todoTasks !! number) todoTasks
-  hPutStr tempHandle $ unlines newTodoItems
-  hClose handle
-  hClose tempHandle
-  removeFile fileName
-  renameFile tempName fileName
+{-- ------------------------------------------------------------------------ --}
+-- import Data.List
+-- import System.Directory
+-- import System.Environment
+-- import System.IO
+-- 
+-- dispatch :: [(String, [String] -> IO ())]
+-- dispatch = [("add", add), ("view", view), ("remove", remove)]
+-- 
+-- main = do
+--   (command:args) <- getArgs
+--   let (Just action) = lookup command dispatch
+--   action args
+-- 
+-- add :: [String] -> IO ()
+-- add [fileName, todoItem] = appendFile fileName (todoItem ++ "\n")
+-- 
+-- view :: [String] -> IO ()
+-- view [fileName] = do
+--   contents <- readFile fileName
+--   let todoTasks = lines contents
+--       numberedTasks =
+--         zipWith (\n line -> show n ++ " - " ++ line) [0 ..] todoTasks
+--   putStr $ unlines numberedTasks
+-- 
+-- remove :: [String] -> IO ()
+-- remove [fileName, numberString] = do
+--   handle <- openFile fileName ReadMode
+--   (tempName, tempHandle) <- openTempFile "." "temp"
+--   contents <- hGetContents handle
+--   let number = read numberString
+--       todoTasks = lines contents
+--       newTodoItems = delete (todoTasks !! number) todoTasks
+--   hPutStr tempHandle $ unlines newTodoItems
+--   hClose handle
+--   hClose tempHandle
+--   removeFile fileName
+--   renameFile tempName fileName
+{-- ------------------------------------------------------------------------ --}
