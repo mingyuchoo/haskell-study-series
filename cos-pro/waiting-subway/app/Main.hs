fb569170521ea7f51a1c124db214ffe30b077e24
mingyuchoo <mingyuchoo@gmail.com>
2021-06-24 02:33:06 +0900

    t570

--- cos-pro/waiting-subway/app/Main.hs
+++ cos-pro/waiting-subway/app/Main.hs
@@ -12,23 +12,50 @@
 {-# LANGUAGE TupleSections              #-}
 {-# LANGUAGE TypeApplications           #-}
 
-module Main( split
-           , main) where
+module Main where
 
+import           Data.List
+
+
+-- "05:31" 331
+-- "11:59" 719
+-- "13:30" 810
+-- "23:32" 1412
+--
+-- "12:00" 720
+--
 split :: String -> [String]
 split [] = [""]
 split (x:xs)
-  | x == ':' = "" : rest
+  | x == ':'  = "" : rest
   | otherwise = (x : head rest) : tail rest
   where
     rest = split xs
 
 
-hello :: [String] -> Int
-hello []          = error "No head for empty lists!"
-hello [""]        = error "No empty string!"
-hello (x : y : _) = 60 * (read x)  + (read y) :: Int
+minutes :: [String] -> Int
+minutes []          = error "No head for empty lists!"
+minutes [""]        = error "No empty string!"
+minutes (x : y : _) = 60 * (read x)  + (read y) :: Int
+
+
+
+diff :: Int -> Int -> Int
+diff x y = y - x
+
+
+min' :: [Int] -> Int
+min' [] = error "No items!"
+min' (x:xs)
+    | x < 0     = min' xs
+    | otherwise = minimum (x:xs)
 
 
 main :: IO ()
-main = putStrLn "Hello, World"
+main = do
+    let currentTime = minutes $ split "12:00"
+        subwayTimes = ["05:31","11:59","13:30","23:32"]
+        mappedTimes = map (\x -> split x) subwayTimes
+        timedTimes  = map (\x -> minutes x) mappedTimes
+    putStrLn $ show $  min' $ map (diff currentTime) timedTimes
+
