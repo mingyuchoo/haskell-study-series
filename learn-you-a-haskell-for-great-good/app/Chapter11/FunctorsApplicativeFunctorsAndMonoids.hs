355852def949908b9e218e6bf08bd08293b28fe7
mingyuchoo <mingyuchoo@gmail.com>
2021-06-28 04:05:06 +0900

    t570

--- learn-you-a-haskell-for-great-good/app/Chapter11/FunctorsApplicativeFunctorsAndMonoids.hs
+++ learn-you-a-haskell-for-great-good/app/Chapter11/FunctorsApplicativeFunctorsAndMonoids.hs
@@ -1,17 +1,3 @@
-{-# LANGUAGE ConstraintKinds            #-}
-{-# LANGUAGE DeriveGeneric              #-}
-{-# LANGUAGE DerivingStrategies         #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE InstanceSigs               #-}
-{-# LANGUAGE KindSignatures             #-}
-{-# LANGUAGE LambdaCase                 #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE RecordWildCards            #-}
-{-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE StandaloneDeriving         #-}
-{-# LANGUAGE TupleSections              #-}
-{-# LANGUAGE TypeApplications           #-}
-
 {-- ------------------------------------------------------------------------ --}
 -- main = do
 --     line <- getLine
@@ -108,11 +94,5 @@
 --                 (Node 8 Empty Empty)
 --                 (Node 10 Empty Empty)
 --             )
-{-- ------------------------------------------------------------------------ --}
-{-- ------------------------------------------------------------------------ --}
-{-- ------------------------------------------------------------------------ --}
-{-- ------------------------------------------------------------------------ --}
-{-- ------------------------------------------------------------------------ --}
-{-- ------------------------------------------------------------------------ --}
 {-- ------------------------------------------------------------------------ --}
 
