19b6264184dcc9d6c2f892476b1aa09de4be8755
mingyuchoo <mingyuchoo@gmail.com>
2021-07-10 06:38:52 +0900

    macbookpro

--- learnyouahaskell/src/Chapter12/AFistfulOfMonads.hs
+++ learnyouahaskell/src/Chapter12/AFistfulOfMonads.hs
@@ -1,5 +1,7 @@
 module Chapter12.AFistfulOfMonads where
 -------------------------------------------------------------------------------
+import           Control.Monad
+-------------------------------------------------------------------------------
 chapter12 :: IO ()
 chapter12 = do
   print $ (*)  <$> Just 2         <*> Just 8
@@ -71,6 +73,28 @@ chapter12 = do
   print $ listOfTuples
   print $ [(n,ch) | n <- [1,2], ch <- ['a','b']]
   print $ [x | x <- [1..50], '7' `elem` show x]
+
+  -- import Control.Monad
+  print $ do guard (5 > 2) :: Maybe ()
+  print $ do guard (1 > 2) :: Maybe ()
+  print $ do guard (5 > 2) :: [()]
+  print $ do guard (1 > 2) :: [()]
+  print $ do [1..50] >>= (\x -> guard ('7' `elem` show x) >> return x)
+  print $ do guard (5 > 2) >> return "cool" :: [String]
+  print $ do guard (1 > 2) >> return "cool" :: [String]
+  print sevensOnly
+
+  -- Left identity
+  print $ return 3 >>= (\x -> Just (x+100000))
+  print $ (\x -> Just (x+100000)) 3
+
+  print $ return "WoM" >>= (\x -> [x,x,x])
+  print $ (\x -> [x,x,x]) "WoM"
+
+  -- Right identity
+  print $ Just "move on up" >>= (\x -> return x)
+  print $ [1,2,3,4]         >>= (\x -> return x)
+  putStrLn "Wah!"           >>= (\x -> return x)
 
 
   return ()
@@ -152,5 +176,46 @@ listOfTuples = do
   ch <- ['a','b']
   return (n, ch)
 -------------------------------------------------------------------------------
+sevensOnly :: [Int]
+sevensOnly = do
+  x <- [1..50]
+  guard ('7' `elem` show x)
+  return x
 -------------------------------------------------------------------------------
+type KnightPos = (Int,Int)
+
+moveKnight :: KnightPos -> [KnightPos]
+moveKnight (c,r) = do
+  (c',r') <- [(c+2,r-1),(c+2,r+1),(c-2,r-1),(c-2,r+1)
+             ,(c+1,r-2),(c+1,r+2),(c-1,r-2),(c-1,r+2)
+             ]
+  guard (c' `elem` [1..8] && r' `elem` [1..8])
+  return (c',r')
+
+
+moveKnight' :: KnightPos -> [KnightPos]
+moveKnight' (c,r) = filter onBoard
+  [(c+2,r-1),(c+2,r+1),(c-2,r-1),(c-2,r+1)
+  ,(c+1,r-2),(c+1,r+2),(c-1,r-2),(c-1,r+2)
+  ]
+  where onBoard (c,r) = c `elem` [1..8] && r `elem` [1..8]
+
+
+in3 :: KnightPos -> [KnightPos]
+in3 start = do
+  first <- moveKnight start
+  second <- moveKnight first
+  moveKnight second
+
+
+in3' start = return start >>= moveKnight >>= moveKnight >>= moveKnight
+
+
+canReachIn3 :: KnightPos -> KnightPos -> Bool
+canReachIn3 start end = end `elem` in3 start
+
+-------------------------------------------------------------------------------
+(<=<) :: (Monad m) => (b -> m c) -> (a -> m b) -> (a -> m c)
+f <=< g = (\x -> g x >>= f)
+
 -------------------------------------------------------------------------------
