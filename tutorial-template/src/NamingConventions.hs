d3c231886f1bbddc687ad94c5613e8547a5cc0d0
mingyuchoo <mingyuchoo@gmail.com>
2021-08-27 05:00:32 +0900

    macbookpro

--- tutorial-template/src/NamingConventions.hs
+++ tutorial-template/src/NamingConventions.hs
@@ -1,20 +1,37 @@
+--------------------------------------------------------------------------------
 module NamingConventions where
 
-data MyType = MyDataConstructor String
+--------------------------------------------------------------------------------
 
-class MyClass a where
+data MyTypeConstructor = MyDataConstructor String
+
+
+-- :kind MyTypeClass :: * -> Constraint
+-- :kind MyTypeClass Int :: Constraint
+-- :kind MyTypeClass MyTypeConstructor :: Constraint
+-- :type name :: MyTypeClass a => a -> String
+class MyTypeClass a where
   name :: a -> String
 
-instance MyClass MyType where
+
+
+-- :type name :: MyTypeClass a => a -> String
+-- :type name (MyDataConstructor "a") :: String
+instance MyTypeClass MyTypeConstructor where
   name (MyDataConstructor name) = name
 
 
+--------------------------------------------------------------------------------
+func :: p -> p
 func x  = x
+
+func' :: p -> p
 func' y = y
 
 
+--------------------------------------------------------------------------------
 solution :: IO ()
 solution = do
-  print $ name  (MyDataConstructor "Haskell")
+  print $ name (MyDataConstructor "Haskell")
   print $ 100 + 200
   print $ (+) 100 200
