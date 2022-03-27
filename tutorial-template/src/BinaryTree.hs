e0cfea8d884feceaa576a4ff614d20e1563c5945
mingyuchoo <mingyuchoo@gmail.com>
2022-03-27 10:35:46 +0900

    updated

--- tutorial-template/src/BinaryTree.hs
+++ tutorial-template/src/BinaryTree.hs
@@ -17,8 +17,18 @@ data Tree a = Leaf
 -- |
 --
 --
-tree :: Tree Int
-tree = Branch 1 (Branch 2 Leaf Leaf) (Branch 3 Leaf Leaf)
+tree1 :: Tree Int
+tree1 = Branch 8
+         (Branch 3
+            (Branch 1 Leaf Leaf)
+            (Branch 6
+              (Branch 4 Leaf Leaf)
+              (Branch 7 Leaf Leaf)))
+         (Branch 10
+            (Branch 9 Leaf Leaf)
+            (Branch 14
+              (Branch 13 Leaf Leaf)
+              Leaf))
 
 -- |
 --
@@ -30,7 +40,7 @@ size (Branch _ l r) = 1 + size l + size r
 -- |
 --
 --
-total :: Tree Int -> Int
+total :: (Num a) => Tree a -> a
 total Leaf           = 0
 total (Branch x l r) = x + total l + total r
 
@@ -48,33 +58,44 @@ maxDepth (Branch _ l r) = 1 + max' (maxDepth l) (maxDepth r)
 -- |
 --
 --
-tList :: Tree a -> [a]
-tList Leaf           = []
-tList (Branch x l r) = [x] ++ tList l ++ tList r
+toList :: Tree a -> [a]
+toList Leaf           = []
+toList (Branch x l r) = toList l ++ [x] ++ toList r
 
 -- |
 --
 --
-tMap :: (a -> b) -> Tree a -> Tree b
-tMap _ Leaf           = Leaf
-tMap f (Branch x l r) = Branch (f x) (tMap f l) (tMap f r)
+map' :: (a -> b) -> Tree a -> Tree b
+map' _ Leaf           = Leaf
+map' f (Branch x l r) = Branch (f x) (map' f l) (map' f r)
 
+tree2 :: Tree (Int,String)
+tree2 = Branch (8,"8")
+         (Branch (3,"3")
+            (Branch (1,"1") Leaf Leaf)
+            (Branch (6,"6")
+              (Branch (4,"4") Leaf Leaf)
+              (Branch (7,"7") Leaf Leaf)))
+         (Branch (10,"10")
+            (Branch (9,"9") Leaf Leaf)
+            (Branch (14,"14")
+              (Branch (13,"13") Leaf Leaf)
+              Leaf))
 -- |
 --
 --
 lookup' :: (Ord a) => Tree (a,b) -> a -> Maybe b
 lookup' Leaf _ = Nothing
-lookup' (Branch (k',v') l r) k
-  | k == k'  = Just v'
-  | otherwise = if k < k' then lookup' l k
-                          else lookup' r k
+lookup' (Branch (k',v') l r) k | k == k'   = Just v'
+                               | k < k'    = lookup' l k
+                               | k > k'    = lookup' r k
+                               | otherwise = Nothing
 
 -- |
 --
 --
 insert :: Ord a => Tree (a,b) -> a -> b -> Tree (a,b)
 insert Leaf k v = Branch (k,v) Leaf Leaf
-insert (Branch (k',v') l r) k v
-  | k == k'   = Branch (k,v) l r
-  | k < k'    = Branch (k',v') (insert l k v)  r
-  | otherwise = Branch (k',v') l (insert r k v)
+insert (Branch (k',v') l r) k v | k < k'    = Branch (k',v') (insert l k v)  r
+                                | k > k'    = Branch (k',v') l (insert r k v)
+                                | otherwise = Branch (k,v) l r
