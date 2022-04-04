a9da432a5babf82a40fcc2ec73dcc6903ae9fe48
mingyuchoo <mingyuchoo@gmail.com>
2022-04-05 05:29:51 +0900

    added

--- cos-pro/greedy-coins/src/Lib.hs
+++ cos-pro/greedy-coins/src/Lib.hs
@@ -1,7 +1,8 @@
 module Lib
     where
 
-import           Debug.Trace
+import qualified Data.Map    as M
+import           Debug.Trace ()
 import           System.IO   (BufferMode (NoBuffering), hSetBuffering, stdout)
 
 
@@ -17,10 +18,14 @@ someFunc = do
 -- When to give a customer change
 -- I want to give change back to the smallest number of coins
 
+type Change = Int
+type Coin = Int
+type Pack = (Coin, Int)
+
 -- |
 --
 --
-calc1 :: Int -> Int -> Int
+calc1 :: Change -> Coin -> Int
 calc1 x y =
   if y >= 500 then calc1 (x + 1) (y - 500) else
   if y >= 100 then calc1 (x + 1) (y - 100) else
@@ -31,10 +36,49 @@ calc1 x y =
 -- |
 --
 --
-calc2 :: Int -> Int -> Int
+calc2 :: Change -> Coin -> Int
 calc2 x y
   | y >= 500 = calc2 (x + 1) (y - 500)
   | y >= 100 = calc2 (x + 1) (y - 100)
   | y >=  50 = calc2 (x + 1) (y -  50)
   | y >=  10 = calc2 (x + 1) (y -  10)
   | otherwise = x
+
+
+-- |
+--
+--
+calc3 :: Change -> [Coin] -> Int
+calc3 change coins =
+  foldl reducer 0 $ func1 change coins []
+
+reducer :: Int -> Pack -> Int
+reducer x y = x + snd y
+
+func1 :: Change -> [Coin] -> [Pack] -> [Pack]
+func1 0 _  packs = packs
+func1 _ [] packs = packs
+func1 x (y:ys) packs
+  | quotient > 0 = func1 remain ys ((y, quotient):packs)
+  | otherwise    = func1 x ys packs
+  where
+    quotient = x `div` y
+    remain = x - y * quotient
+
+
+-- |
+--
+--
+calc4 :: Change -> [Coin] -> Int
+calc4 change coins =
+  sum $ M.elems $ func2 change coins M.empty
+
+func2 :: Change -> [Coin] -> M.Map Coin Int -> M.Map Coin Int
+func2 0 _  m = m
+func2 _ [] m = m
+func2 x (y:ys) m
+  | quotient > 0 = func2 remain ys (M.insert y quotient m)
+  | otherwise    = func2 x ys m
+  where
+    quotient = x `div` y
+    remain = x - y * quotient
