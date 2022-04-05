3768892104d1955fb46821584d80ea1a8da5c2f5
mingyuchoo <mingyuchoo@gmail.com>
2022-04-06 05:49:04 +0900

    updated

--- cos-pro/biggest-sum/src/Lib.hs
+++ cos-pro/biggest-sum/src/Lib.hs
@@ -1,8 +1,10 @@
 module Lib
     where
 
+import           Data.List
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = print $ solution list
 
 
 -- Given a list of `N` numbers
@@ -12,3 +14,41 @@ someFunc = putStrLn "someFunc"
 -- When generate a sum value with the list
 -- and the constrains
 -- Then I should get the biggest sum value.
+
+-- |
+--
+--
+list :: [Int]
+list = [2,4,5,4,6]
+
+n :: Int
+n = length list
+
+m :: Int
+m = 8
+
+k :: Int
+k = 3
+
+
+-- |
+--
+--
+solution :: [Int] -> Int
+solution xs =
+   (first xs * k * quotient) + (second xs * remainder)
+    where
+      rsort :: [Int] -> Int -> Int
+      rsort l x = (reverse . sort) l !! x
+
+      first :: [Int] -> Int
+      first l = rsort l 0
+
+      second :: [Int] -> Int
+      second l = rsort l 1
+
+      quotient :: Int
+      quotient = div m k
+
+      remainder :: Int
+      remainder = mod m k
