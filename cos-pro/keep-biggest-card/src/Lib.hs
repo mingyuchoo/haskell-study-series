a6431c999873d7e8c804447000e49df56edd7881
mingyuchoo <mingyuchoo@gmail.com>
2022-04-07 05:19:12 +0900

    updated

--- cos-pro/keep-biggest-card/src/Lib.hs
+++ cos-pro/keep-biggest-card/src/Lib.hs
@@ -1,9 +1,13 @@
 module Lib
-    ( someFunc
-    ) where
+    where
+
+import           Data.List (sort, transpose)
+import           Flow      ((<|))
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  print <| solution1 matrix1
+  print <| solution1 matrix2
 
 -- I want to get the largest number
 -- from the result of
@@ -13,13 +17,52 @@ someFunc = putStrLn "someFunc"
 -- N: the number of rows
 -- N x M: matrix of numbers
 
+
+-- |
+--
+--
+n1 :: Int
 n1 = 3
+
+m1 :: Int
 m1 = 3
+
+matrix1 :: [[Int]]
 matrix1 = [[3,1,2],[4,1,4],[2,2,2]]
-anwer1 = 2
+
+answer1 :: Int
+answer1 = 2
 
 
+-- |
+--
+--
+n2 :: Int
 n2 = 2
+
+m2 :: Int
 m2 = 4
+
+matrix2 :: [[Int]]
 matrix2 = [[27,3,1,8],[3,3,3,4]]
-anwer2 = 3
+
+answer2 :: Int
+answer2 = 3
+
+
+-- |
+--
+--
+solution1 :: [[Int]] -> Int
+solution1 matrix =
+  last <| head <| transpose <| map sort matrix
+
+
+-- |
+--
+--
+solution2 :: [[Int]] -> Int
+solution2 matrix =
+  foldl max 0 <| getMin matrix
+  where
+    getMin = map (foldl min 99999)
--- cos-pro/keep-biggest-card/test/Spec.hs
+++ cos-pro/keep-biggest-card/test/Spec.hs
@@ -1,2 +1,31 @@
+import Lib
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "Given matrix1" $ do
+    describe "When apply `solution1` function to maxtrix1" $ do
+      describe "Then it" $ do
+        it "Should be evaluated as answer1" $ do
+          (solution1 matrix1) `shouldBe` answer1
+
+  describe "Given matrixw" $ do
+    describe "When apply `solution1` function to maxtrix2" $ do
+      describe "Then it" $ do
+        it "Should be evaluated as answer2" $ do
+          (solution1 matrix2) `shouldBe` answer2
+
+  describe "Given matrix1" $ do
+    describe "When apply `solution2` function to maxtrix1" $ do
+      describe "Then it" $ do
+        it "Should be evaluated as answer1" $ do
+          (solution2 matrix1) `shouldBe` answer1
+
+  describe "Given matrixw" $ do
+    describe "When apply `solution2` function to maxtrix2" $ do
+      describe "Then it" $ do
+        it "Should be evaluated as answer2" $ do
+          (solution2 matrix2) `shouldBe` answer2
