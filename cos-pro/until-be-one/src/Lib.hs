fe16009ba86dba66d0e1fde9bd2c4319803a5eb6
mingyuchoo <mingyuchoo@gmail.com>
2022-04-07 17:54:34 +0900

    updated

--- cos-pro/until-be-one/src/Lib.hs
+++ cos-pro/until-be-one/src/Lib.hs
@@ -6,7 +6,7 @@ someFunc = putStrLn "someFunc"
 
 {-
 N (2 <= N <= 100,000)
-K (2 <= K <= 100,100)
+K (2 <= K <= 100,000)
 
 OP1) N - 1
 OP2) N / K
@@ -18,5 +18,11 @@ When run solution
 Then it should be 2
 -}
 
-solution :: Int
-solution = undefined
+solution :: Int -> Int -> Int
+solution = calc 0
+  where
+    calc :: Int -> Int -> Int -> Int
+    calc _   _ 0 = error "Error: divided by 0"
+    calc acc 1 _ = acc
+    calc acc x y | x `mod` y == 0 = calc (acc + 1) (x `div` y) y
+                 | otherwise      = calc (acc + 1) (x - 1) y
