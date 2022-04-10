a97fc347b567e20c4237f58f79619152ae65c9ab
mingyuchoo <mingyuchoo@gmail.com>
2022-04-11 06:38:30 +0900

    updated

--- cos-pro/check-number-three/src/Lib.hs
+++ cos-pro/check-number-three/src/Lib.hs
@@ -10,33 +10,36 @@ someFunc :: IO ()
 someFunc = do
   putStr "Input hour to find out including the number 3> "
   number <- getLine
-  print . length <| find [ hour <> minute <> second
-                         | hour <- hours (read number)
-                         , minute <- minutes
-                         , second <- seconds
-                         ]
-
+  print <| solution1 (read number)
+  print <| solution2 (read number)
 
 -- |
 --
 --
-hours :: Int -> [String]
-hours x = map show [0..x]
+solution1 :: Int -> Int
+solution1 n =
+  length <| find [ hour <> minute <> second
+                 | hour   <- hours n
+                 , minute <- minutes
+                 , second <- seconds
+                 ]
 
--- |
---
---
-minutes :: [String]
-minutes = map show [0..59]
+  where
+    hours :: Int -> [String]
+    hours x = map show [0..x]
 
--- |
---
---
-seconds :: [String]
-seconds = map show [0..59]
+    minutes :: [String]
+    minutes = map show [0..59]
 
--- |
---
---
-find :: [String] -> [String]
-find = filter (elem '3')
+    seconds :: [String]
+    seconds = map show [0..59]
+
+    find :: [String] -> [String]
+    find = filter (elem '3')
+
+
+solution2 :: Int -> Int
+solution2 x = ((x+1) - nums x) * ((60-15)*15 + 15*60) + (nums x * 60 * 60)
+  where
+    nums :: Int -> Int
+    nums h = length <| filter (elem '3') <| map show [0..h]
