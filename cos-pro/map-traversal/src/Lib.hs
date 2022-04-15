6992e414f7a3995c00be223cde90599c17ae6dce
mingyuchoo <mingyuchoo@gmail.com>
2022-04-15 14:57:08 +0900

    updated

--- cos-pro/map-traversal/src/Lib.hs
+++ cos-pro/map-traversal/src/Lib.hs
@@ -1,16 +1,16 @@
 module Lib
     where
 
-import           Debug.Trace
 
 type Matrix = [[Int]]
 
-type Coord =
-  ( Int -- ^ row
-  , Int -- ^ column
-  , Int -- ^ direction
-  )
+data Coord = Coord { row       :: Int
+                   , column    :: Int
+                   , direction :: Int
+                   }
 
+instance Show Coord where
+  show c@(Coord x y z) = show x <> show y <> show z
 -- |
 --
 --
@@ -18,13 +18,8 @@ someFunc :: IO ()
 someFunc =
   putStrLn "Hello, World"
 
-size :: (Int, Int)
-size =
-  (4,4)
-
 start :: Coord
-start =
-  (1,1,0)
+start = Coord 1 1 0
 
 matrix :: Matrix
 matrix =
@@ -35,41 +30,56 @@ matrix =
   ]
 
 
-checkNextDirection :: Matrix -> Coord -> (Int, Int)
-checkNextDirection m c@(x, y, _) =
-  let
-    next = newDirection $ snd $ checkDirection m c
-  in
-    checkDirection m (x, y, next)
-
--- -----------------------------------------------------------------------------
--- |
---
---
-checkDirection :: Matrix -> Coord -> (Int, Int)
-checkDirection m c =
-  getValueAt m $ getFwd c
-
-getValueAt :: Matrix -> Coord -> (Int, Int)
-getValueAt m (x, y, d) =
-  (m !! x !! y, d)
-
-getFwd :: Coord -> Coord
-getFwd (x, y, d) =
-  (x+u, y+v, d)
+-- ----------------------------------------------------------------------------
+getFirst :: Matrix -> Coord -> (Int, Coord)
+getFirst m c@(Coord x y z) =
+  head $ filter predicate values
   where
-    (u, v) = forward !! d
-    forward =
-      [ (-1, 0)
-      , ( 0, 1)
-      , ( 1, 0)
-      , ( 0,-1)
-      ]
--- -----------------------------------------------------------------------------
+    values = valuesFrom m c
+    predicate w = fst w == 0
 
+
+
+valuesFrom :: Matrix -> Coord -> [(Int, Coord)]
+valuesFrom m c =
+  map fn (mkFwd c)
+  where
+    fn :: Coord -> (Int, Coord)
+    fn c@(Coord x y d) = (m !! x !! y, c)
+
+
+mkFwd :: Coord -> [Coord]
+mkFwd (Coord x y d) =
+  zipWith mkPos ps ds
+  where
+    ps = map (\w -> forward !! w) ds
+    ds = directions d
+    mkPos :: (Int, Int) -> Int -> Coord
+    mkPos (u, v) = Coord (x+u) (y+v)
+
+forward :: [(Int, Int)]
+forward =
+  [ (-1, 0) -- 0: North
+  , ( 0, 1) -- 1: East
+  , ( 1, 0) -- 2: South
+  , ( 0,-1) -- 3: West
+  ]
+
+directions :: Int -> [Int]
+directions = mkSeq 4 []
+  where
+    mkSeq :: Int -> [Int] -> Int -> [Int]
+    mkSeq 0 l d = let rl = reverse l in last rl : init rl
+    mkSeq n l d = mkSeq (n-1) (newDirection d:l) (newDirection d)
+
+newDirection :: Int -> Int
+newDirection x =
+  mod (x + 3) 4
+
+-- -----------------------------------------------------------------------------
 checkBwd :: Coord -> Coord
-checkBwd (x, y, d) =
-  (x+u, y+v, d)
+checkBwd (Coord x y d) =
+  Coord (x+u) (y+v) d
   where
     (u, v) = backward !! d
     backward =
@@ -78,16 +88,3 @@ checkBwd (x, y, d) =
       , (-1, 0)
       , ( 0, 1)
       ]
-
-dirs :: Int -> [Int]
-dirs = mkSeq 4 []
-  where
-    mkSeq :: Int -> [Int] -> Int -> [Int]
-    mkSeq 0 l d = d : reverse l
-    mkSeq n l d = mkSeq (n-1) (newDirection d:l) (newDirection d)
-
-
-
-newDirection :: Int -> Int
-newDirection x =
-  mod (x + 3) 4
