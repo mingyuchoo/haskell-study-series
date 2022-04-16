4c19e4b3cb055ab97962a99d9c3128e4cefcf4ac
mingyuchoo <mingyuchoo@gmail.com>
2022-04-17 08:26:41 +0900

    updated

--- cos-pro/map-traversal/src/Lib.hs
+++ cos-pro/map-traversal/src/Lib.hs
@@ -1,19 +1,25 @@
 module Lib
     where
 
+import           Control.Lens (element, (&), (.~))
 
+type Direct = Int
+type Value = Int
+type Row = Int
+type Col = Int
 type Matrix = [[Int]]
-
-data Coord = Coord { row       :: Int
-                   , column    :: Int
-                   , direction :: Int
+data Coord = Coord { row       :: Row
+                   , column    :: Col
+                   , direction :: Direct
                    }
 
 instance Show Coord where
-  show c@(Coord x y z) = show x <> show y <> show z
--- |
---
---
+  show c@(Coord x y z) =
+    "<" <> show x <>
+    "," <> show y <>
+    "," <> show z <>
+    ">"
+
 someFunc :: IO ()
 someFunc =
   putStrLn "Hello, World"
@@ -29,24 +35,54 @@ matrix =
   , [1,1,1,1]
   ]
 
+xMatrix :: Matrix
+xMatrix =
+  [[1,1,1,1],[1,2,2,1],[1,1,0,1],[1,1,1,1]]
 
+yValue :: Value
+yValue =
+  0
+
+zCoord :: Coord
+zCoord =
+  Coord 2 2 2
+
+driver :: (Matrix, Value, Coord)
+driver =
+  moveFwd (xMatrix, yValue, zCoord)
 -- ----------------------------------------------------------------------------
-getFirst :: Matrix -> Coord -> (Int, Coord)
-getFirst m c@(Coord x y z) =
-  head $ filter predicate values
+moveFwd :: (Matrix, Value, Coord) -> (Matrix, Value, Coord)
+moveFwd (m, _, c@(Coord x y d)) =
+    (visit m c, v, n)
   where
-    values = valuesFrom m c
+    (v, n) = filterFwd m c
+
+filterFwd :: Matrix -> Coord -> (Value, Coord)
+filterFwd m c =
+  let
     predicate w = fst w == 0
+    values = valuesFrom mkFwd m c
+  in
+    case filter predicate values of
+      [] -> filterBwd m c
+      vs -> head vs
 
+filterBwd :: Matrix -> Coord -> (Value, Coord)
+filterBwd m c =
+  let
+    predicate w = fst w /= 1
+    values = valuesFrom mkBwd m c
+  in
+    case filter predicate values of
+      [] -> (0, Coord 0 0 0)
+      vs -> head vs
 
-
-valuesFrom :: Matrix -> Coord -> [(Int, Coord)]
-valuesFrom m c =
-  map fn (mkFwd c)
+valuesFrom :: (Coord -> [Coord]) -> Matrix -> Coord -> [(Value, Coord)]
+valuesFrom f m c =
+  map fn (f c)
   where
-    fn :: Coord -> (Int, Coord)
+    fn :: Coord -> (Value, Coord)
     fn c@(Coord x y d) = (m !! x !! y, c)
-
 
 mkFwd :: Coord -> [Coord]
 mkFwd (Coord x y d) =
@@ -54,37 +90,49 @@ mkFwd (Coord x y d) =
   where
     ps = map (\w -> forward !! w) ds
     ds = directions d
-    mkPos :: (Int, Int) -> Int -> Coord
+
+    mkPos :: (Row, Col) -> Direct -> Coord
     mkPos (u, v) = Coord (x+u) (y+v)
 
-forward :: [(Int, Int)]
-forward =
-  [ (-1, 0) -- 0: North
-  , ( 0, 1) -- 1: East
-  , ( 1, 0) -- 2: South
-  , ( 0,-1) -- 3: West
-  ]
+    forward :: [(Row, Col)]
+    forward =
+      [ (-1, 0) -- 0: North
+      , ( 0, 1) -- 1: East
+      , ( 1, 0) -- 2: South
+      , ( 0,-1) -- 3: West
+      ]
 
-directions :: Int -> [Int]
+mkBwd :: Coord -> [Coord]
+mkBwd (Coord x y d) =
+  zipWith mkPos ps ds
+  where
+    ps = map (\w -> backward !! w) ds
+    ds = directions d
+
+    mkPos :: (Row, Col) -> Direct -> Coord
+    mkPos (u, v) = Coord (x+u) (y+v)
+
+    backward :: [(Row, Col)]
+    backward =
+      [ ( 1, 0) -- 0: North
+      , ( 0,-1) -- 1: East
+      , (-1, 0) -- 2: South
+      , ( 0, 1) -- 3: West
+      ]
+
+directions :: Direct -> [Direct]
 directions = mkSeq 4 []
   where
-    mkSeq :: Int -> [Int] -> Int -> [Int]
+    mkSeq :: Direct -> [Direct] -> Direct -> [Direct]
     mkSeq 0 l d = let rl = reverse l in last rl : init rl
     mkSeq n l d = mkSeq (n-1) (newDirection d:l) (newDirection d)
 
-newDirection :: Int -> Int
+newDirection :: Direct -> Direct
 newDirection x =
   mod (x + 3) 4
 
 -- -----------------------------------------------------------------------------
-checkBwd :: Coord -> Coord
-checkBwd (Coord x y d) =
-  Coord (x+u) (y+v) d
-  where
-    (u, v) = backward !! d
-    backward =
-      [ ( 1, 0)
-      , ( 0,-1)
-      , (-1, 0)
-      , ( 0, 1)
-      ]
+
+visit :: Matrix -> Coord -> Matrix
+visit m c@(Coord x y _) = m & element x . element y .~ 2
+
