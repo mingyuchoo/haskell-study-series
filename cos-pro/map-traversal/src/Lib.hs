86488c8c8d30d3bfec37fa9f2be96483f0900db3
mingyuchoo <mingyuchoo@gmail.com>
2022-04-17 19:03:41 +0900

    updated

--- cos-pro/map-traversal/src/Lib.hs
+++ cos-pro/map-traversal/src/Lib.hs
@@ -2,6 +2,7 @@ module Lib
     where
 
 import           Control.Lens (element, (&), (.~))
+import           Data.Maybe   (mapMaybe)
 
 type Direct = Int
 type Value = Int
@@ -14,19 +15,29 @@ data Coord = Coord { row       :: Row
                    }
 
 instance Show Coord where
-  show c@(Coord x y z) =
+  show (Coord x y d) =
     "<" <> show x <>
     "," <> show y <>
-    "," <> show z <>
+    "," <> show d <>
     ">"
 
+-- |
+--
+--
 someFunc :: IO ()
 someFunc =
-  putStrLn "Hello, World"
+  print $ moveFwd (matrix, direction start, start) 0
 
+-- |
+--
+--
 start :: Coord
-start = Coord 1 1 0
+start =
+  Coord 1 1 0
 
+-- |
+--
+--
 matrix :: Matrix
 matrix =
   [ [1,1,1,1]
@@ -35,28 +46,19 @@ matrix =
   , [1,1,1,1]
   ]
 
-xMatrix :: Matrix
-xMatrix =
-  [[1,1,1,1],[1,2,2,1],[1,1,0,1],[1,1,1,1]]
-
-yValue :: Value
-yValue =
-  0
-
-zCoord :: Coord
-zCoord =
-  Coord 2 2 2
-
-driver :: (Matrix, Value, Coord)
-driver =
-  moveFwd (xMatrix, yValue, zCoord)
--- ----------------------------------------------------------------------------
-moveFwd :: (Matrix, Value, Coord) -> (Matrix, Value, Coord)
-moveFwd (m, _, c@(Coord x y d)) =
-    (visit m c, v, n)
+-- |
+--
+--
+moveFwd :: (Matrix, Value, Coord) -> Int -> Int
+moveFwd (m, _, c) acc
+  | v == -1 = acc
+  | otherwise = moveFwd (visit m c, v, n) (acc + 1)
   where
     (v, n) = filterFwd m c
 
+-- |
+--
+--
 filterFwd :: Matrix -> Coord -> (Value, Coord)
 filterFwd m c =
   let
@@ -67,23 +69,39 @@ filterFwd m c =
       [] -> filterBwd m c
       vs -> head vs
 
+-- |
+--
+--
 filterBwd :: Matrix -> Coord -> (Value, Coord)
-filterBwd m c =
+filterBwd m c@(Coord _ _ d) =
   let
-    predicate w = fst w /= 1
     values = valuesFrom mkBwd m c
   in
-    case filter predicate values of
-      [] -> (0, Coord 0 0 0)
+    case mapMaybe (canGoBack d) values of
+      [] -> (-1, Coord (-1)(-1)(-1))
       vs -> head vs
 
+-- |
+--
+--
+canGoBack :: Direct -> (Value, Coord) -> Maybe (Value, Coord)
+canGoBack d c@(v, Coord _ _ z)
+  | d == z && v /= 1 = Just c
+  | otherwise = Nothing
+
+-- |
+--
+--
 valuesFrom :: (Coord -> [Coord]) -> Matrix -> Coord -> [(Value, Coord)]
 valuesFrom f m c =
   map fn (f c)
   where
     fn :: Coord -> (Value, Coord)
-    fn c@(Coord x y d) = (m !! x !! y, c)
+    fn a@(Coord x y _) = (m !! x !! y, a)
 
+-- |
+--
+--
 mkFwd :: Coord -> [Coord]
 mkFwd (Coord x y d) =
   zipWith mkPos ps ds
@@ -92,7 +110,7 @@ mkFwd (Coord x y d) =
     ds = directions d
 
     mkPos :: (Row, Col) -> Direct -> Coord
-    mkPos (u, v) = Coord (x+u) (y+v)
+    mkPos (u, v) = Coord (x + u) (y + v)
 
     forward :: [(Row, Col)]
     forward =
@@ -102,6 +120,9 @@ mkFwd (Coord x y d) =
       , ( 0,-1) -- 3: West
       ]
 
+-- |
+--
+--
 mkBwd :: Coord -> [Coord]
 mkBwd (Coord x y d) =
   zipWith mkPos ps ds
@@ -110,7 +131,7 @@ mkBwd (Coord x y d) =
     ds = directions d
 
     mkPos :: (Row, Col) -> Direct -> Coord
-    mkPos (u, v) = Coord (x+u) (y+v)
+    mkPos (u, v) = Coord (x + u) (y + v)
 
     backward :: [(Row, Col)]
     backward =
@@ -120,19 +141,27 @@ mkBwd (Coord x y d) =
       , ( 0, 1) -- 3: West
       ]
 
+-- |
+--
+--
 directions :: Direct -> [Direct]
-directions = mkSeq 4 []
+directions =
+  mkSeq 4 []
   where
     mkSeq :: Direct -> [Direct] -> Direct -> [Direct]
-    mkSeq 0 l d = let rl = reverse l in last rl : init rl
-    mkSeq n l d = mkSeq (n-1) (newDirection d:l) (newDirection d)
+    mkSeq 0 l _ = let rl = reverse l in last rl : init rl
+    mkSeq n l d = mkSeq (n - 1) (newDirection d : l) (newDirection d)
 
+-- |
+--
+--
 newDirection :: Direct -> Direct
 newDirection x =
   mod (x + 3) 4
 
--- -----------------------------------------------------------------------------
-
+-- |
+--
+--
 visit :: Matrix -> Coord -> Matrix
-visit m c@(Coord x y _) = m & element x . element y .~ 2
-
+visit m (Coord x y _) =
+  m & element x . element y .~ 2
---
+++ cos-pro/map-traversal/src/Recurs.hs
@@ -0,0 +1,27 @@
+module Recurs
+    where
+
+import           Debug.Trace (trace)
+
+-- |
+--
+--
+func1 :: Int -> Int ->Int
+func1 x y =
+  case x of
+    0 -> trace ("DEBUG 1> x: " <> show x <> ", y:" <> show y) y
+    _ -> trace ("DEBUG 2> x: " <> show x <> ", y:" <> show y) func1 (x - 1) (y + 1)
+
+-- |
+--
+--
+func2 :: Int -> Int ->Int
+func2 x y | x == 0    = trace ("DEBUG 1> x: " <> show x <> ", y:" <> show y) y
+          | otherwise = trace ("DEBUG 2> x: " <> show x <> ", y:" <> show y) func2 (x - 1) (y + 1)
+
+-- |
+--
+--
+func3 :: Int -> Int ->Int
+func3 0 y = trace ("DEBUG 1> x: " <> show 0 <> ", y:" <> show y) y
+func3 x y = trace ("DEBUG 2> x: " <> show x <> ", y:" <> show y) func3 (x - 1) (y + 1)
