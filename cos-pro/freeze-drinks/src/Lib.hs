df9d95e11da9fe4a9771661927ba9c00ad0f7ac8
mingyuchoo <mingyuchoo@gmail.com>
2022-04-19 06:33:22 +0900

    updated

--- cos-pro/freeze-drinks/src/Lib.hs
+++ cos-pro/freeze-drinks/src/Lib.hs
@@ -1,7 +1,6 @@
 module Lib
     where
 
-import           Data.Maybe
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -9,35 +8,54 @@ someFunc = putStrLn "someFunc"
 
 -- -----------------------------------------------------------------------------
 
-newtype Stack a = Stack [a]
-                deriving (Show)
+newtype Stack a =
+  Stack [a] deriving (Show)
 
 emptyS :: Stack a
 emptyS = Stack []
 
-push :: a -> Stack a -> Stack a
-push x (Stack xs) = Stack (x:xs)
+-- |
+--
+-- >>> emptyS `push` 1 `push` 2 `push` 3
+-- Stack [3,2,1]
+--
+push :: Stack a -> a -> Stack a
+push (Stack xs) x = Stack (x:xs)
 
+-- |
+--
+-- >>> pop $ Stack[3,2,1]
+-- (Just 3,Stack [2,1])
+--
 pop :: Stack a -> (Maybe a, Stack a)
 pop (Stack [])     = (Nothing, Stack [])
 pop (Stack (x:xs)) = (Just x, Stack xs)
 
 -- -----------------------------------------------------------------------------
 
-newtype Queue a = Queue [a]
-                deriving (Show)
+newtype Queue a =
+  Queue [a] deriving (Show)
 
 
 emptyQ :: Queue a
 emptyQ = Queue []
 
 
-enqueue :: a -> Queue a -> Queue a
-enqueue x (Queue xs) = Queue (xs <> [x])
+-- |
+--
+-- >>> emptyQ `enque` 1 `enque` 2 `enque` 3
+-- Stack [1,2,3]
+--
+enque :: Queue a -> a -> Queue a
+enque (Queue xs) x = Queue (xs <> [x])
 
-
-dequeue :: Queue a -> (Maybe a, Queue a)
-dequeue (Queue [])     = (Nothing, Queue [])
-dequeue (Queue (x:xs)) = (Just x, Queue xs)
+-- |
+--
+-- >>> deque $ Queue[1,2,3]
+-- (Just 1,Queue [2,3])
+--
+deque :: Queue a -> (Maybe a, Queue a)
+deque (Queue [])     = (Nothing, Queue [])
+deque (Queue (x:xs)) = (Just x, Queue xs)
 
 -- -----------------------------------------------------------------------------
