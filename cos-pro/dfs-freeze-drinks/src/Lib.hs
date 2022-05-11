241395d6d53404c813d472f6680c7ee3149f6c94
mingyuchoo <mingyuchoo@gmail.com>
2022-05-12 05:11:04 +0900

    updated

--- cos-pro/dfs-freeze-drinks/src/Lib.hs
+++ cos-pro/dfs-freeze-drinks/src/Lib.hs
@@ -4,9 +4,12 @@ module Lib
 
 import           Control.Lens (element, (&), (.~))
 import           Data.Maybe   (mapMaybe)
+import           Flow         ((<|))
 
 someFunc :: IO ()
-someFunc = print driver
+someFunc = do
+  print <| snd <| driver matrix1
+  print <| snd <| driver matrix2
 
 -- -----------------------------------------------------------------------------
 -- 1. 사방 탐색하기
@@ -19,11 +22,18 @@ someFunc = print driver
 type Matrix :: *
 type Matrix = [[Int]]
 
-matrix :: Matrix
-matrix = [ [0,0,1]
-         , [0,1,0]
-         , [1,0,1]
-         ]
+matrix1 :: Matrix
+matrix1 = [ [0,0,1]
+          , [0,1,0]
+          , [1,0,1]
+          ]
+
+matrix2 :: Matrix
+matrix2 = [ [0,0,1,1,0]
+          , [0,0,0,1,1]
+          , [1,1,1,1,1]
+          , [0,0,0,0,0]
+          ]
 
 type Coordinate :: *
 type Coordinate = (Int, Int)
@@ -31,12 +41,12 @@ type Coordinate = (Int, Int)
 -- 1. 사방 탐색하기
 -- 3. 범위 밖이면 무시하기
 
-getPos :: Matrix -> Coordinate -> [Coordinate]
-getPos m p@(i,j) =
+getPos :: Int -> Int -> Matrix -> Coordinate -> [Coordinate]
+getPos x y m p@(i,j) =
     mapMaybe func dirs
   where
-    rows = 3
-    cols = 3
+    rows = x
+    cols = y
     dirs :: [Coordinate]
     dirs = [ ( 0,-1) -- North
            , ( 0, 1) -- South
@@ -56,12 +66,14 @@ getPos m p@(i,j) =
 type Accumulate :: *
 type Accumulate = (Matrix, Int)
 
-driver :: Accumulate
-driver =
-  foldl fun (matrix, 0) [(i,j) | i <- [0..2], j <- [0..2]]
+driver :: Matrix -> Accumulate
+driver m =
+  foldl func (m, 0) [(i,j) | i <- [0..row-1], j <- [0..col-1]]
   where
-    fun :: Accumulate -> (Int, Int) -> Accumulate
-    fun acc@(m, c) p =
+    row = length m
+    col = length <| head m
+    func :: Accumulate -> (Int, Int) -> Accumulate
+    func acc@(m, c) p =
       if result
         then (newM, c+1)
         else (newM, c)
@@ -76,7 +88,10 @@ travel m p = visit' (m, False) [p]
 
 visit' :: (Matrix, Bool) ->  [Coordinate] -> (Matrix, Bool)
 visit' v []            = v
-visit' v@(m, b) (c:cs) = visit' (visit' (visitMatrix v c) (getPos m c)) cs
+visit' v@(m, b) (c:cs) = visit' (visit' (visitMatrix v c) (getPos row col m c)) cs
+  where
+    row = length m
+    col = length <| head m
 
 -- -----------------------------------------------------------------------------
 -- 4. 방문 기록하기
@@ -85,4 +100,4 @@ visitMatrix :: (Matrix, Bool) -> Coordinate -> (Matrix, Bool)
 visitMatrix v@(m,b) (x,y) =
   case m !! x !! y of
     0 -> (m & element x . element y .~ 2, True)
-    _ -> (m, False)
+    _ -> (m, b)
