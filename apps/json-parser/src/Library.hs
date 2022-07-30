4fbb6e294ecd930b413be1f6f7f6d8edec00fc34
mingyuchoo <mingyuchoo@gmail.com>
2022-07-30 19:02:16 +0900

    updated

--- apps/json-parser/src/Library.hs
+++ apps/json-parser/src/Library.hs
@@ -2,11 +2,13 @@ module Library
     where
 
 import           Data.Aeson              (FromJSON (..), ToJSON (..), (.:))
+import           RIO                     (Generic, IO, LByteString, Text, (<$>))
+
 import qualified Data.Aeson              as JSON
 import qualified Network.HTTP.Client     as HTTP
+
 import           Network.HTTP.Client.TLS (newTlsManager)
 import           Prelude
-import           RIO                     (Generic, IO, LByteString, Text, (<$>))
 
 data User = User { userId       :: !Int
                  , userName     :: !Text
@@ -15,13 +17,21 @@ data User = User { userId       :: !Int
                  } deriving (Eq, Show, Generic)
 
 instance ToJSON User where
-  toJSON = JSON.genericToJSON JSON.defaultOptions
+  toJSON = JSON.genericToJSON $ jsonOptions "user"
+
+
+jsonOptions :: String -> JSON.Options
+jsonOptions prefix =
+  let prefixLength = length prefix
+   in JSON.defaultOptions { JSON.fieldLabelModifier = drop prefixLength
+                          }
 
 getUsersContent :: IO LByteString
 getUsersContent = do
   manager <- newTlsManager
   request <- HTTP.parseRequest "https://jsonplaceholder.typicode.com/users"
   HTTP.responseBody <$> HTTP.httpLbs request manager
+
 
 runMain :: IO ()
 runMain = do
