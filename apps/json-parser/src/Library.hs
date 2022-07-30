92f8d47042767dc75f889cb2709b64b1b62352a0
mingyuchoo <mingyuchoo@gmail.com>
2022-07-31 06:32:00 +0900

    improved

--- apps/json-parser/src/Library.hs
+++ apps/json-parser/src/Library.hs
@@ -2,28 +2,74 @@ module Library
     where
 
 import           Data.Aeson              (FromJSON (..), ToJSON (..), (.:))
-import           RIO                     (Generic, IO, LByteString, Text, (<$>))
-
 import qualified Data.Aeson              as JSON
+import           Data.Char               (toLower)
 import qualified Network.HTTP.Client     as HTTP
-
 import           Network.HTTP.Client.TLS (newTlsManager)
 import           Prelude
+import           RIO
+    ( Generic
+    , IO
+    , LByteString
+    , Text
+    , (<$>)
+    , (>>>)
+    )
 
 data User = User { userId       :: !Int
                  , userName     :: !Text
                  , userUsername :: !Text
                  , userEmail    :: !Text
+                 , userAddress  :: !Address
+                 , userPhone    :: !Text
+                 , userWebsite  :: !Text
+                 , userCompany  :: !Company
                  } deriving (Eq, Show, Generic)
 
-instance ToJSON User where
-  toJSON = JSON.genericToJSON $ jsonOptions "user"
 
+instance FromJSON User where
+  parseJSON = JSON.genericParseJSON $ jsonOptions "user"
+
+-- instance ToJSON User where
+--   toJSON = JSON.genericToJSON $ jsonOptions "user"
+
+data Address = Address { addressStreet  :: !Text
+                       , addressSuite   :: !Text
+                       , addressCity    :: !Text
+                       , addressZipcode :: !Text
+                       , addressGeo     :: !Geo
+                       } deriving (Eq, Show, Generic)
+
+instance FromJSON Address where
+  parseJSON = JSON.genericParseJSON $ jsonOptions "address"
+
+
+data Geo = Geo { geoLat :: !Float
+               , geoLng :: !Float
+               } deriving (Eq, Show, Generic)
+
+instance FromJSON Geo where
+  parseJSON =  JSON.withObject "Geo" $ \x -> do
+    latString <- x .: "lat"
+    lngString <- x .: "lng"
+    let geoLat = read latString
+        geoLng = read lngString
+    return Geo {geoLat, geoLng}
+
+data Company = Company { companyName        :: !Text
+                       , companyCatchPhrase :: !Text
+                       , companyBs          :: !Text
+                       } deriving (Eq, Show, Generic)
+
+instance FromJSON Company where
+  parseJSON = JSON.genericParseJSON $ jsonOptions "company"
 
 jsonOptions :: String -> JSON.Options
 jsonOptions prefix =
   let prefixLength = length prefix
-   in JSON.defaultOptions { JSON.fieldLabelModifier = drop prefixLength
+      lowercaseFirstCharacter (x:xs) = toLower x : xs
+      lowercaseFirstCharacter []     = []
+   in JSON.defaultOptions { JSON.fieldLabelModifier = drop prefixLength >>> lowercaseFirstCharacter
                           }
 
 getUsersContent :: IO LByteString
@@ -31,6 +77,12 @@ getUsersContent = do
   manager <- newTlsManager
   request <- HTTP.parseRequest "https://jsonplaceholder.typicode.com/users"
   HTTP.responseBody <$> HTTP.httpLbs request manager
+
+getUsers :: IO (Either String [User])
+getUsers = do
+  manager <- newTlsManager
+  request <- HTTP.parseRequest "https://jsonplaceholder.typicode.com/users"
+  (HTTP.responseBody >>> JSON.eitherDecode) <$> HTTP.httpLbs request manager
 
 
 runMain :: IO ()
