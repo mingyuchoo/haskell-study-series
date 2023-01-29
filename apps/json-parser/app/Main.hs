5247854f9edb0869a30ad0b7c687cb9274ef420a
mingyuchoo <mingyuchoo@gmail.com>
2023-01-29 14:37:13 +0900

    fixed

--- apps/json-parser/app/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import qualified Library
-import Prelude
-
--- This `main` function just delegates to the library's definition of `main`
-main :: IO ()
-main = Library.runMain
--- apps/json-parser/dev-scripts/run-hoogle.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env sh
-
-hoogle serve --local --host localhost --port 8888 --no-security-headers
