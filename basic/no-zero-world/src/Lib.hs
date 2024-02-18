e5079fb624e49870bd84b1a77e83ffe323013dda
mingyuchoo <mingyuchoo@gmail.com>
2024-02-18 14:52:34 +0900

    updated

--- basic/no-zero-world/src/Lib.hs
+++ basic/no-zero-world/src/Lib.hs
@@ -2,5 +2,17 @@ module Lib
     ( someFunc
     ) where
 
+import           Flow ((<|))
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  print <| converter 9949999 1
+  return ()
+
+converter :: Int -> Int -> Int
+converter x1 x2 =
+  read <| concatMap replaceZero . show <| x1 + x2
+  where
+    replaceZero :: Char -> [Char]
+    replaceZero '0' = ['1']
+    replaceZero c   = [c]
