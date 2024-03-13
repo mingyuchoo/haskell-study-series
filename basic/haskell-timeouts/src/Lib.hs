11c20f58cf7ae6a6311211f2124c45c7cdf2ea37
mingyuchoo <mingyuchoo@gmail.com>
2024-03-14 04:29:47 +0900

    add haskell-timeouts

---
+++ basic/haskell-timeouts/src/Lib.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE LambdaCase         #-}
+{-# LANGUAGE NumericUnderscores #-}
+
+module Lib
+    ( someFunc
+    ) where
+
+import           Control.Concurrent          (forkIO, threadDelay)
+import           Control.Concurrent.STM      (atomically, retry)
+import           Control.Concurrent.STM.TVar (newTVar, readTVar, writeTVar)
+import           Data.Foldable               (asum)
+
+someFunc :: IO ()
+someFunc = do
+  -- -----------------------------------------------------------------
+  result <- atomically $ newTVar Nothing
+  forkIO $ do
+    threadDelay 2_000_000
+    atomically $ writeTVar result (Just "Task A: Completed in two seconds")
+
+  timeout <- atomically $ newTVar False
+  forkIO $ do
+    threadDelay 1_000_000
+    atomically $ writeTVar timeout True
+
+  message <- atomically $ asum [ readTVar result >>= \case Nothing -> retry
+                                                           Just x -> return x
+                               , readTVar timeout >>= \case False -> retry
+                                                            True -> return "Task A: Gave up after one second"
+                               ]
+
+  putStrLn message
+
+  -- -----------------------------------------------------------------
+  result <- atomically $ newTVar Nothing
+  forkIO $ do
+    threadDelay 500_000
+    atomically $ writeTVar result (Just "Task B: Completed in half a second")
+
+  timeout <- atomically $ newTVar False
+  forkIO $ do
+    threadDelay 1_000_000
+    atomically $ writeTVar timeout True
+
+  message <- atomically $ asum [ readTVar result >>= \case Nothing -> retry
+                                                           Just x -> return x
+                               , readTVar timeout >>= \case False -> retry
+                                                            True -> return "Task B: Gave up after one second"
+                               ]
+  putStrLn message
