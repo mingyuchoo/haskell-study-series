1afb300241b25f2916cb95f739fa4cb9ea10dc97
mingyuchoo <mingyuchoo@gmail.com>
2024-03-14 04:55:11 +0900

    added

---
+++ basic/haskell-hashing/src/Lib.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE DeriveAnyClass     #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE DerivingStrategies #-}
+
+module Lib
+    ( someFunc
+    ) where
+
+import           Data.Hashable (Hashable (hash))
+import           Data.Word     (Word8)
+import           GHC.Generics  (Generic)
+
+data Color = Color { red   :: Word8
+                   , green :: Word8
+                   , blue  :: Word8
+                   }
+             deriving stock (Show, Eq, Generic)
+             deriving anyclass (Hashable)
+
+someFunc :: IO ()
+someFunc =
+  print (hash (Color 255 0 0)) >>
+  print (hash (Color 0 255 0)) >>
+  print (hash (Color 0 0 255))
