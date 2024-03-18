9664bd520c5a9aac4171a9abe40b38819c6262b0
mingyuchoo <mingyuchoo@gmail.com>
2024-03-19 03:41:41 +0900

    updated

--- cos-pro/palindromes/src/Lib.hs
+++ cos-pro/palindromes/src/Lib.hs
@@ -1,14 +1,40 @@
 module Lib
     where
 
+import           Data.Char
+
 isPalindrome :: String -> Bool
-isPalindrome word =
-  word == myReverse word
+isPalindrome x =
+  x == myReverse x
 
 myReverse :: [Char] -> [Char]
 myReverse [] = []
 myReverse xs = myReverse (tail xs) ++ head xs : []
 
+maybePalindrome :: String -> Maybe Bool
+maybePalindrome "" = Nothing
+maybePalindrome x  = Just $ isPalindrome x
+
+verbose :: String -> String
+verbose x =
+  case maybePalindrome x of
+    Nothing    -> "Please enter a word."
+    Just False -> "Sorry, this is not a palindrome."
+    Just True  -> "Yay, it's a palindrome!"
+
+ignoreCase :: String -> String
+ignoreCase = map toLower
+
+makeWord :: String -> String
+makeWord = filter isAlphabet
+  where
+    isAlphabet :: Char  -> Bool
+    isAlphabet c | c `elem` ['a' .. 'z'] = True
+                 | c `elem` ['A' .. 'Z'] = True
+                 | otherwise = False
+
 someFunc :: IO ()
 someFunc = do
-  print $ isPalindrome "mom"
+  putStr "Input a word to check if it is a palindrome: "
+  x <- getLine
+  print $ maybePalindrome $ makeWord $ ignoreCase x
--- cos-pro/palindromes/test/Spec.hs
+++ cos-pro/palindromes/test/Spec.hs
@@ -10,13 +10,21 @@ main = hspec spec
 spec :: Spec
 spec = do
     describe "Given Prelude" $ do
-        context "when use `read` function" $ do
-            it "should parse integers" $ do
-                read "10" `shouldBe` (10 :: Int)
-            it "should parse floating-point numbers" $ do
-                read "2.5" `shouldBe` (2.5 :: Float)
+      context "when use `read` function" $ do
+        it "should parse integers" $ do
+          read "10" `shouldBe` (10 :: Int)
+        it "should parse floating-point numbers" $ do
+          read "2.5" `shouldBe` (2.5 :: Float)
     describe "Given Lib" $ do
-        context "when use `isPalindrome` function" $ do
-            it "should be succeeded" $ do
-              isPalindrome "bob" `shouldBe` True
-              isPalindrome "hello" `shouldBe` False
+      context "when use `isPalindrome` function" $ do
+        it "should be succeeded" $ do
+          isPalindrome "bob" `shouldBe` True
+          isPalindrome "hello" `shouldBe` False
+      context "when use `ignoreCase` function" $ do
+        it "should be succeeded" $ do
+          ignoreCase "abcdefg" `shouldBe` "abcdefg"
+          ignoreCase "aBcDeFg" `shouldBe` "abcdefg"
+          ignoreCase "ABCDEFG" `shouldBe` "abcdefg"
+      context "when use `makeWord` function" $ do
+        it "should be succededed" $ do
+          makeWord "madam, I'm Adam!" `shouldBe` "madamImAdam"
