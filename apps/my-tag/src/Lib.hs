ed05501f1e1fd77a1b412c52ca9f377ccf6a9f3d
mingyuchoo <mingyuchoo@gmail.com>
2024-08-13 03:20:14 +0900

    added

---
+++ apps/my-tag/src/Lib.hs
@@ -0,0 +1,28 @@
+module Lib
+    ( someFunc
+    ) where
+
+import           System.IO          ()
+import           Text.Parsec        (anyChar, manyTill, parse, string, try)
+import           Text.Parsec.String (Parser)
+
+
+someFunc :: IO ()
+someFunc = do
+  input <- readFile "input.txt"
+  let output = convertTag input
+  writeFile "output.tsx" output
+  putStrLn "Completed: output.tsx"
+
+
+convertTag :: String -> String
+convertTag input = case parse parseTitle "" input of
+  Left _     -> input
+  Right html -> html
+
+
+parseTitle :: Parser String
+parseTitle = do
+  _ <- string "<제목>"
+  content <- manyTill anyChar (try (string "</제목>"))
+  return $ "<MainTitle>" ++ content ++ "</MainTitle>"
