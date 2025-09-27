29037921c6bd4fd83ff67716238aac4225b0e195
mingyuchoo <mingyuchoo@address.com>
2025-09-28 07:07:11 +0900

    refactor: replace function composition with Flow's <| operator for better readability

--- Database/postgresql-simple-init/src/Lib.hs
+++ Database/postgresql-simple-init/src/Lib.hs
@@ -5,7 +5,7 @@ module Lib
 import           Database.PostgreSQL.Simple
 import           Database.PostgreSQL.Simple.FromRow
 import           Database.PostgreSQL.Simple.ToRow
-
+import           Flow ((<|))
 -- | Test data type
 data TestField = TestField { testId   :: Int
                            , testName :: String
@@ -41,33 +41,33 @@ someFunc = do
     initDB conn
 
     cid <- createTest conn
-    putStrLn $ "New Test: " ++ (show cid)
+    putStrLn <| "New Test: " <> (show cid)
 
     updated <- updateTest conn
-    putStrLn $ "Ret value: " ++ (show updated)
+    putStrLn <| "Ret value: " <> (show updated)
 
     row <- retrieveTest conn
     mapM_ print row
 
     deleted <- deleteTest conn
-    putStrLn $ "Ret value: " ++ (show deleted)
+    putStrLn <| "Ret value: " <> (show deleted)
 
 -- | Create a new test
 createTest :: Connection -> IO [Only Int]
-createTest conn = query conn "INSERT INTO test (id, name) VALUES (?, ?) RETURNING id" $ ((1 :: Int), ("Jacob" :: String))
+createTest conn = query conn "INSERT INTO test (id, name) VALUES (?, ?) RETURNING id" <| ((1 :: Int), ("Jacob" :: String))
 
 -- | Update a test
 updateTest :: Connection -> IO Bool
 updateTest conn = do
-    n <- execute conn "UPDATE test SET name = ? WHERE id = ?" $ (("Tomas" :: String), (1 :: Int))
-    return $ n > 0
+    n <- execute conn "UPDATE test SET name = ? WHERE id = ?" <| (("Tomas" :: String), (1 :: Int))
+    return <| n > 0
 
 -- | Retrieve a test
 retrieveTest :: Connection -> IO  [TestField]
-retrieveTest conn = query conn "SELECT id, name FROM test WHERE id = ?" $ (Only (1 :: Int))
+retrieveTest conn = query conn "SELECT id, name FROM test WHERE id = ?" <| (Only (1 :: Int))
 
 -- | Delete a test
 deleteTest :: Connection -> IO Bool
 deleteTest conn = do
-    n <- execute conn "DELETE FROM test WHERE id = ?" $ (Only (1 :: Int))
-    return $ n > 0
+    n <- execute conn "DELETE FROM test WHERE id = ?" <| (Only (1 :: Int))
+    return <| n > 0
