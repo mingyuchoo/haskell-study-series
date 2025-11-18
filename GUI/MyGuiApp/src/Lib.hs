87621d587650d839d4b4a2e0f051d9f380401d77
mingyuchoo <mingyuchoo@gmail.com>
2025-11-18 21:11:57 +0900

    Added

---
+++ GUI/MyGuiApp/src/Lib.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+
+module Lib
+    ( someFunc
+    ) where
+
+import qualified GI.Gtk as Gtk
+import Data.GI.Base
+
+someFunc :: IO ()
+someFunc = do
+    -- Application 생성
+    app <- new Gtk.Application
+        [ #applicationId := "com.example.MyGuiApp" ]
+
+    -- Application activate
+    on app #activate $ do
+        window <- new Gtk.ApplicationWindow
+            [ #application := app
+            , #title := "Hello GTK4"
+            , #defaultWidth := 300
+            , #defaultHeight := 200
+            ]
+
+        button <- new Gtk.Button
+            [ #label := "Click me" ]
+
+        -- 클릭 이벤트
+        on button #clicked $
+            putStrLn "Button clicked!"
+
+        -- GTK4에서는 Widget upcast 필요
+        widget <- Gtk.toWidget button
+
+        #setChild window (Just widget)
+
+        #show window
+
+    _ <- #run app Nothing
+    return ()
