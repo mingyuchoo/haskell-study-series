a8c33b18e3b474af755ac8e6ad83a0194d49667d
mingyuchoo <mingyuchoo@address.com>
2025-11-19 16:36:17 +0900

    feat(GUI): Add MonomerTodo project with Monomer GUI framework
    - Create new MonomerTodo project directory with complete Monomer application structure
    - Add project configuration files (cabal, stack, package.yaml, Setup.hs)
    - Add GHCi configuration (.ghci) with enhanced REPL settings including color output, hlint integration, and hoogle support
    - Add GHCid configuration (.ghcid) for live reloading during development
    - Add macOS-specific configuration (Info.plist) for graphics switching and high resolution support
    - Add project metadata (LICENSE, README.md)
    - Create TodoTypes module for todo application data structures
    - Create Main.hs with Monomer application entry point and event handling
    - Add asset directories with Roboto fonts and Remix icon font for UI styling
    - Add .gitignore to exclude build artifacts and IDE files
    - Improve .ghci configuration in existing MonomerApp project with better GHCi options
    - Refactor function signatures in MonomerApp/src/Main.hs for better readability

--- GUI/MonomerApp/src/Main.hs
+++ GUI/MonomerApp/src/Main.hs
@@ -22,10 +22,9 @@ data AppEvent = AppInit | AppIncrease
 
 makeLenses 'AppModel
 
-buildUI
-  :: WidgetEnv AppModel AppEvent
-  -> AppModel
-  -> WidgetNode AppModel AppEvent
+buildUI :: WidgetEnv AppModel AppEvent
+        -> AppModel
+        -> WidgetNode AppModel AppEvent
 buildUI wenv model = widgetTree where
   widgetTree = vstack [
       label "Hello world",
@@ -37,12 +36,11 @@ buildUI wenv model = widgetTree where
       ]
     ] `styleBasic` [padding 10]
 
-handleEvent
-  :: WidgetEnv AppModel AppEvent
-  -> WidgetNode AppModel AppEvent
-  -> AppModel
-  -> AppEvent
-  -> [AppEventResponse AppModel AppEvent]
+handleEvent :: WidgetEnv AppModel AppEvent
+            -> WidgetNode AppModel AppEvent
+            -> AppModel
+            -> AppEvent
+            -> [AppEventResponse AppModel AppEvent]
 handleEvent wenv node model evt = case evt of
   AppInit     -> []
   AppIncrease -> [Model (model & clickCount +~ 1)]
---
+++ GUI/MonomerTodo/src/TodoTypes.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+
+module TodoTypes
+    where
+
+import           Control.Lens.TH
+
+import           Data.Default
+import           Data.Text       (Text)
+
+import           Monomer
+
+data TodoType = Home | Work | Sports
+     deriving (Enum, Eq, Show)
+
+data TodoStatus = Pending | Done
+     deriving (Enum, Eq, Show)
+
+data Todo = Todo { _todoId      :: Millisecond
+                 , _todoType    :: TodoType
+                 , _status      :: TodoStatus
+                 , _description :: Text
+                 }
+     deriving (Eq, Show)
+
+instance Default Todo where
+  def = Todo {
+    _todoId = 0,
+    _todoType = Home,
+    _status = Pending,
+    _description = ""
+  }
+
+data TodoAction = TodoNone
+                | TodoAdding
+                | TodoEditing Int
+                | TodoConfirmingDelete Int Todo
+     deriving (Eq, Show)
+
+data TodoModel = TodoModel { _todos      :: [Todo]
+                           , _activeTodo :: Todo
+                           , _action     :: TodoAction
+                           }
+     deriving (Eq, Show)
+
+data TodoEvt = TodoInit
+             | TodoNew
+             | TodoAdd
+             | TodoEdit Int Todo
+             | TodoSave Int
+             | TodoConfirmDelete Int Todo
+             | TodoCancelDelete
+             | TodoDeleteBegin Int Todo
+             | TodoDelete Int Todo
+             | TodoShowEdit
+             | TodoHideEdit
+             | TodoHideEditDone
+             | TodoCancel
+     deriving (Eq, Show)
+
+makeLenses 'TodoModel
+makeLenses 'Todo
+
+todoTypes :: [TodoType]
+todoTypes = enumFrom (toEnum 0)
+
+todoStatuses :: [TodoStatus]
+todoStatuses = enumFrom (toEnum 0)
